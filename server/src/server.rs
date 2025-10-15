fd023243f31778e7a2106e25947545320ed2818f
M Mahrous <mdms@odoo.com>
2025-10-15 14:07:41 +0200

    [IMP] allow vars in diagnostic filters and log parsing errors

--- server/src/server.rs
+++ server/src/server.rs
@@ -125,6 +125,7 @@ impl Server {
         if let Some(initialize_params) = initialize_params.process_id {
             self.client_process_id = initialize_params;
         }
+        #[allow(deprecated)]
         if let Some(workspace_folders) = initialize_params.workspace_folders {
             let sync_odoo = self.sync_odoo.lock().unwrap();
             let file_mgr = sync_odoo.get_file_mgr();
